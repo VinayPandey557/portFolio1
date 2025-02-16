20766492b96c815e8b10175e1045bd9e795b4864
Vinay Pandey <vinaypandey46158@gmail.com>
2025-02-16 23:20:46 +0530

    Added_more_components

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body
-        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden`}
-      >
+        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
+      > 
         {children}
       </body>
     </html>
